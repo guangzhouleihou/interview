8f342867b4d8e3bdaccca61c8d717d3378ceb0a5
unknown <>
2018-09-05 16:33:48 +0800

    fotmat -李泽浩 date 2018-9-5

--- src/main/java/com/leihou/interview/InterviewApplication.java
+++ src/main/java/com/leihou/interview/InterviewApplication.java
@@ -3,7 +3,11 @@ package com.leihou.interview;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,12 +20,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @MapperScan("com.leihou.interview.mapper")
 @ComponentScan("com.leihou")
-@EnableTransactionManagement
-@EnableScheduling
-@SpringBootApplication
-@EnableAsync
+//@EnableTransactionManagement
+//@EnableScheduling
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
+//@EnableAsync
 @EnableSwagger2
-public class InterviewApplication {
+public class InterviewApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(InterviewApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InterviewApplication.class, args);
